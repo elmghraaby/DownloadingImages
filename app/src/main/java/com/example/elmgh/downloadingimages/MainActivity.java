1b30bc7a3c83d9b476bc1f57849b5334e548a884
Ahmed Alaa <ahmed.alaa50.aa@gmail.com>
2018-07-05 01:16:29 +0200

    Initial commit

---
+++ app/src/main/java/com/example/elmgh/downloadingimages/MainActivity.java
@@ -0,0 +1,106 @@
+package com.example.elmgh.downloadingimages;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.Image;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+/*
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+*/
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.ExecutionException;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+        ImageView downloadedImg;
+
+        public void downloadImage(View view) {
+
+            // https://upload.wikimedia.org/wikipedia/en/a/aa/Bart_Simpson_200px.png
+
+            ImageDownloader task = new ImageDownloader();
+            Bitmap myImage;
+
+            try {
+                myImage = task.execute("https://metrouk2.files.wordpress.com/2018/07/gettyimages-989021338.jpg").get();
+
+                downloadedImg.setImageBitmap(myImage);
+
+            } catch (Exception e) {
+
+                e.printStackTrace();
+
+            }
+
+            Log.i("Interaction", "Button Tapped");
+
+        }
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+
+            downloadedImg = (ImageView) findViewById(R.id.imageView);
+
+        }
+
+        public class ImageDownloader extends AsyncTask<String, Void, Bitmap> {
+
+
+            @Override
+            protected Bitmap doInBackground(String... urls) {
+
+                try {
+
+                    URL url = new URL(urls[0]);
+
+                    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+                    connection.connect();
+
+                    InputStream inputStream = connection.getInputStream();
+
+                    Bitmap myBitmap = BitmapFactory.decodeStream(inputStream);
+
+                    return myBitmap;
+
+
+                } catch (MalformedURLException e) {
+
+                    e.printStackTrace();
+
+                } catch (IOException e) {
+
+                    e.printStackTrace();
+
+                }
+
+                return null;
+
+            }
+
+
+        }
+
+}
